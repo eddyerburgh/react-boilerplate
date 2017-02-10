0b0256fbd7b8d626ad2c80080001e5006fe0ded9
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-02-10 21:05:12 +0000

    Move babel presets out to .babelrc and remove babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
       },
       {
         test: /\.scss$/,
